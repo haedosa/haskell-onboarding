a64b09b8f32deb932ac516c77c89e59ebba01940
Suwon Park <suwonpark@haedosa.xyz>
2024-07-12 10:38:27 +0900

    [refactor] remove non-exercism exercises

--- algorithm-design-with-haskell/src/Kata.hs
+++
@@ -1,4 +0,0 @@
-module Kata where
-
-import Kata.Ch1MyAnswer
-import Kata.Ch1ExMyAnswer
--- learn-you-a-haskell/app/first.hs
+++
@@ -1,9 +0,0 @@
-import First
-import System.Environment (getArgs)
-
-main :: IO ()
-main = do
-  [x] <- getArgs
-  let z = read x :: Double
-      y = doubleNum z
-  print y
